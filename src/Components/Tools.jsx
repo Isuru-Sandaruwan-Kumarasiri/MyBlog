6b916124cb6ac861793ca34e4d4e19347a69ae7a
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-06-11 22:27:58 +0530

    creaetd frontend file upload url and imge upload url

--- src/Components/Tools.jsx
+++ src/Components/Tools.jsx
@@ -9,13 +9,61 @@ import Marker from "@editorjs/marker";
 import Inlinecode from "@editorjs/inline-code";
 
 
+import { UploadImage } from "../Common/AWS";
+
+
+
+
+
+const uploadImageByFile=(e)=>{
+     return  UploadImage(e).then(url=>{
+        if(url){
+            return {
+                success:1,
+                file:{url}
+            }
+        }
+      })
+}
+
+
+
+const uploadImageByURL=(e)=>{
+
+    let link =new Promise((resolve,reject)=>{
+
+        try {
+            resolve(e);
+            
+        } catch (error) {
+            reject(err);
+        }
+    })
+    return link.then(url=>{
+        return{
+            success :1,
+            file:{url}
+        }
+    });
+}
+
+
+
 export const Tools ={
     embed :Embed,
     list:{
         class:List,
         inlineToolbar:true
     },
-    image:Image,
+    image:{
+        class:Image,
+        config:{
+            uploader:{
+                uploadByUrl:uploadImageByURL,
+                 uploadByFile:uploadImageByFile
+            }
+        }
+    },
     header:{
         class:Header,
         config:{
