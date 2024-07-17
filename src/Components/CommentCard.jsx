2a9e855a5d3056eae447ac7da194e387b5cb47fb
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-07-17 23:31:16 +0530

    replying to commnet

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,11 +1,30 @@
+import { useContext, useState } from "react";
 import { getDay } from "../Common/Date";
+import { UserContext } from "../App";
+import toast, { Toaster } from "react-hot-toast";
+import CommentField from "./CommentField";
 
 const CommentCard=({index,leftVal,commentData})=>{
 
-    let {commented_by:{personal_info:{profile_img,fullname,username}},commentedAt,comment}=commentData;
+    let {commented_by:{personal_info:{profile_img,fullname,username}},commentedAt,comment,_id}=commentData;
+
+    let {userAuth:{access_token}}=useContext(UserContext);
+
+    const [isReplaying,setReplaying]=useState(false);
+
+    const handleReply=()=>{
+        if(!access_token){
+            return toast.error("Login first to leave a reply")
+        }
+        setReplaying(preVal=>!preVal);
+
+
+    }
 
     return(
+        
         <div className="w-full " style={{paddingLeft:`${leftVal*10}px`}}>
+            
 
             <div className="my-5 p-6 rounded-md border border-grey">
                   <div className="flex gap-3 items-center mb-8">
@@ -16,9 +35,20 @@ const CommentCard=({index,leftVal,commentData})=>{
 
                   <p className="font-gelasio text-xl ml-3">{comment}</p>
 
-                  <div>
-                      
+                  <div className="flex gap-5 items-center mt-5">
+                    <button 
+                        className="underline"
+                        onClick={handleReply}
+                        >Reply
+                        </button>
                   </div>
+                  {
+                    isReplaying ?
+                    <div className="mt-8">
+                          <CommentField action="reply" index={index} replyingTo={_id} setReplying={setReplaying}/>
+                    </div>
+                    :""
+                  }
             </div>
 
         </div>
--- src/Components/CommentField.jsx
+++ src/Components/CommentField.jsx
@@ -4,7 +4,7 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import { BlogContext } from "../Pages/BlogPage";
 
-const CommentField = ({ action }) => {
+const CommentField = ({ action,index=undefined,replyingTo=undefined,setReplying }) => {
   let {
     blog,
     blog: {
@@ -41,6 +41,7 @@ const CommentField = ({ action }) => {
           _id,
           blog_author,
           comment,
+          replying_to:replyingTo
         },
         {
           headers: {
@@ -57,11 +58,29 @@ const CommentField = ({ action }) => {
 
         let newCommentArr;
 
-        data.childrenLevel = 0;
+        if(replyingTo){
+          
+          commentsArr[index].children.push(data._id);
 
-        newCommentArr = [data,...commentsArr];
+          data.childrenLevel=commentsArr[index].childrenLevel+1;
+          data.parentIndex=index;
 
-        let parentCommentIncrement = 1;
+          commentsArr[index].isReplyLoaded=true;
+
+          commentsArr.splice(index +1,0,data);
+
+          newCommentArr=commentsArr;
+
+          setReplying(false)
+
+        }else{
+          data.childrenLevel = 0;
+          newCommentArr = [data,...commentsArr];
+        }
+
+        
+
+        let parentCommentIncrement = replyingTo ? 0 : 1;
 
         setBlog({
           ...blog,
