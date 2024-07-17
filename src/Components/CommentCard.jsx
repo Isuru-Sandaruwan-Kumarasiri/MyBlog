d58eae5195d4f1403d4abaadf2e0b86833cba963
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-07-18 00:30:29 +0530

    hide reply button

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -3,14 +3,40 @@ import { getDay } from "../Common/Date";
 import { UserContext } from "../App";
 import toast, { Toaster } from "react-hot-toast";
 import CommentField from "./CommentField";
+import { BlogContext } from "../Pages/BlogPage";
+
+
+
 
 const CommentCard=({index,leftVal,commentData})=>{
 
     let {commented_by:{personal_info:{profile_img,fullname,username}},commentedAt,comment,_id}=commentData;
 
+    let {blog,blog:{comments:{results:commentsArr}},setBlog}=useContext(BlogContext)
+
     let {userAuth:{access_token}}=useContext(UserContext);
 
     const [isReplaying,setReplaying]=useState(false);
+
+    const removeCommentsCards=(startingPoint)=>{
+                
+        if(commentsArr[startingPoint]){
+
+            while(commentsArr[startingPoint].childrenLevel>commentData.childrenLevel){
+                commentsArr.splice(startingPoint,1);
+                if(!commentsArr[startingPoint]){
+                    break;
+                }
+            }
+        }
+        setBlog({...blog,comments:{results:commentsArr}})
+    }
+
+    const hideReplies=()=>{
+
+        commentData.isReplyLoaded=false;
+        removeCommentsCards(index +1);
+    }
 
     const handleReply=()=>{
         if(!access_token){
@@ -36,6 +62,16 @@ const CommentCard=({index,leftVal,commentData})=>{
                   <p className="font-gelasio text-xl ml-3">{comment}</p>
 
                   <div className="flex gap-5 items-center mt-5">
+
+                    {
+                        commentData.isReplyLoaded ? 
+                            <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+                                    onClick={hideReplies}
+                            
+                            >
+                            <i className="fi fi-rs-comment-dots"></i> Hide Reply
+                            </button> :""
+                    }
                     <button 
                         className="underline"
                         onClick={handleReply}
